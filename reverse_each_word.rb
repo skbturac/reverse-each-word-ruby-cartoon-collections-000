8159bed0efc46e294ae8155c5a93b849d086a916
Suheyl Turac <skbsturac@live.com>
2018-09-24 20:23:46 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word("Hello there, and how are you?")
+  
+  array = ("Hello there, and how are you?").split(" ")
+  
+  array.collect do |array|
+    array.reverse!
+    
+end
+  array.join(" ")
+  
+end
